13d17ba2f387078df3870db9cd7aa587a0391fe6
DhuandharCoder <pt4597591@gmail.com>
2026-09-09 22:44:00 +0530

    Checks the biggest question of universe.

---
+++ deep.py
@@ -0,0 +1,6 @@
+ques = input("what is the Great Question of Life, the Universe, and Everything? ")
+if ques.strip().lower() == "42" or ques.strip().lower() == "forty-two" or ques.strip().lower() == "forty two":
+
+    print("Yes")
+else:
+    print("No")
