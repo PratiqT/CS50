a2de1bc792d0d21a0697d88bb8afaa57a591e3ec
DhuandharCoder <pt4597591@gmail.com>
2026-09-11 22:01:54 +0530

    Show the list entered without repetition

---
+++ 4/grocery.py
@@ -0,0 +1,18 @@
+items = {}
+while True:
+    try:
+        item = input("").upper()
+        if item in items:
+            items[item] += 1
+        else:
+            items[item] = 1
+    except EOFError:
+            break
+
+for item in sorted(items):
+    print(items[item], item)
+
+
+
+
+
