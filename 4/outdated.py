a168231367343159d9fb1f873bfd26e64a0c6362
DhuandharCoder <pt4597591@gmail.com>
2026-09-11 22:04:32 +0530

    Converts the entered date in DD/MM/YYYY format to YYYY-MM-DD

--- 4/outdated.py
+++ 4/outdated.py
@@ -1,1 +1,29 @@
+months = [
+    "January", "February", "March", "April",
+    "May", "June", "July", "August",
+    "September", "October", "November", "December"
+]
 
+while True:
+    date = input("Date: ")
+
+    try:
+        if "/" in date:
+            month, day, year = date.split("/")
+        else:
+            if "," not in date:
+                continue
+
+            month_name, day, year = date.split(" ")
+            day = day.replace(",", "")
+            month = months.index(month_name) + 1
+
+        month = int(month)
+        day = int(day)
+
+        if 1 <= month <= 12 and 1 <= day <= 31:
+            print(f"{year}-{month:02}-{day:02}")
+            break
+
+    except ValueError:
+        pass
