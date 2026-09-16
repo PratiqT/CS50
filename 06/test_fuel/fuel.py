33b2cb86b75142ae50028453676ad0f0cc5e3ae2
DhuandharCoder <pt4597591@gmail.com>
2026-09-16 22:34:19 +0530

    This is feul.py form lec04

---
+++ 06/test_fuel/fuel.py
@@ -0,0 +1,28 @@
+def main():
+    fraction = input("Fraction: ")
+    percentage = convert(fraction)
+    print(gauge(percentage))
+def convert(fraction):
+    try:
+        x, y = fraction.split("/")
+        x = int(x)
+        y = int(y)
+    except ValueError:
+        raise ValueError
+
+    if y == 0:
+        raise ZeroDivisionError
+
+    if x > y or x < 0 or y < 0:
+        raise ValueError
+
+    return round(x / y * 100)
+def gauge(percentage):
+    if percentage >= 99:
+        return "F"
+    elif percentage <= 1:
+        return "E"
+    else:
+        return f"{percentage}%"
+if __name__ == "__main__":
+    main()
